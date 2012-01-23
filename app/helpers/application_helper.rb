2769698336f854d74c433b8816b062b4f8e20068
Linh Mai <linhmt@sgsoftechnology.com>
2012-01-23 23:56:48 +0700

    check if no active important notice

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,11 @@
 module ApplicationHelper
   def current_important_notice
     imp_notice = Notice.where(:is_active => true).first
-    imp_notice.content
+    if imp_notice.nil?
+      notice = "Check out!!!"
+    else
+      notice = imp_notice.content
+    end
+    notice
   end
 end
