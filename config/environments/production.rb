e95b6375b1f983a3720da92b68c19f0de6ed02d4
Linh Mai <linhmt@sgsoftechnology.com>
2012-01-23 23:59:30 +0700

    fix precompile error with new production setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,19 +9,19 @@ Dvhk::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
-  config.assets.compress = false
+  config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
 
   # Defaults to Rails.root.join("public/assets")
-  # config.assets.manifest = YOUR_PATH
+  #  config.assets.manifest = Rails.root.join("public/assets")
 
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
@@ -43,7 +43,9 @@ Dvhk::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+#  config.assets.precompile += %w( web-app-theme/base.css web-app-theme/themes/blue/style.css )
+  config.assets.precompile += %w( web_app_theme.css )
+  
 
 
   # Disable delivery errors, bad email addresses will be ignored
