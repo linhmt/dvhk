be8fb79f347d1a2114d3263405d9b81def5130d1
Linh Mai-Office <linhmt@vietnamairlines.com>
2012-06-06 17:11:33 +0700

    add new file reading

--- app/models/data_file.rb
+++ app/models/data_file.rb
@@ -8,37 +8,47 @@ class DataFile < ActiveRecord::Base
     File.open(path, "wb") { |f| f.write(upload['datafile'].read) }
   end
   
+  
+  # Start from 1 because split creates empty string
   def self.read(upload)
     path = File.join("public/data", upload)
     File.open(path, 'r') {|f|
       lines = f.readlines("\n")
+      flight_date = Date.parse(upload.chomp('.txt'))
       lines.each do |line|
         a = line.split(%r{[|\s]+})
         if (a.length > 5 && a[3].match(/VN.*/))
-          route = Routing.find_by_routing a[4]
-          if route.nil?
-            route = Routing.new
-            route.routing = a[4]
-            route.is_arrival = true
-            route.destination = "Sai Gon"
-            route.save!
-          end
           flight = ArrivalFlight.new
-          aircraft_d = a[2].split('-')
-          aircraft = Aircraft.find_by_reg_no(aircraft_d[1])
-          if (aircraft.nil?)
-            aircraft = Aircraft.new
-            aircraft.aircraft_type = aircraft_d[0]
-            aircraft.reg_no = aircraft_d[1]
-            aircraft.save
-          end
-          flight.reg_no = aircraft_d[1]
           flight.flight_no = a[3]
-          flight.flight_date = Date.today
+          route = Routing.find_or_create_by_routing(a[4], {:is_arrival => true, :destination => "Sai Gon"})
           flight.routing_id = route.id
+          aircraft_d = a[2].split('-')
+          if (aircraft_d.size == 2)
+            reg_no = aircraft_d[1]
+            air_type = aircraft_d[0]
+          elsif (aircraft_d.size == 3)
+            reg_no = aircraft_d[2]
+            air_type = aircraft_d[1]
+          else
+            reg_no = '999'
+            air_type = aircraft_d[0]
+          end
+          Aircraft.find_or_create_by_reg_no(reg_no, {:aircraft_type => air_type})
+          flight.reg_no = reg_no
+          time_raw = a[5].split(':')
+          if (!a[6].nil? && a[6] == 'ARRNEXTDAY')
+            sta_time = Time.local(flight_date.year, flight_date.month, flight_date.day + 1, time_raw[0], time_raw[1])            
+            flight.flight_date = flight_date.advance(:days => 1)
+          else
+            sta_time = Time.local(flight_date.year, flight_date.month, flight_date.day, time_raw[0], time_raw[1])
+            flight.flight_date = flight_date
+          end
+          flight.sta = sta_time
+          flight.remarks = a[6]
           flight.save!
         end
       end
     }
+    path
   end
 end
