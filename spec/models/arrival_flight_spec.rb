7da1ab9984a15c74a920bc5763ec0fccd136f6ce
Linh Mai-Office <linhmt@vietnamairlines.com>
2012-06-18 20:18:58 +0700

    new rspec

--- spec/models/arrival_flight_spec.rb
+++ spec/models/arrival_flight_spec.rb
@@ -4,16 +4,19 @@ describe ArrivalFlight do
   before(:each) do
     @user = Factory(:user)
     @attr = {
-      :flight_no => "VN5213",
+      :flight_no => "VN1213",
       :flight_date => Date.today,
-      :sta => DateTime.now
+      :sta => DateTime.now,
+      :eta => DateTime.now,
+      :ata => DateTime.now,
+      :ssr => "chuyen bay nhieu VIP/CIP"
     }
   end
   
   describe "A authenticated user creates new arrival flight" do
     it "generate flight with flight number, assigned user_id, flight_date" do
       a_flight = @user.arrival_flights.create(@attr)
-      a_flight.save
+      a_flight.save!
       a_flight.user_id.should == @user.id
       a_flight.flight_date.to_date.should == Date.today
       a_flight.flight_no.should_not be_nil
@@ -41,6 +44,34 @@ describe ArrivalFlight do
       @flight.sta = nil
       @flight.save
       @flight.should_not be_valid
+    end
+  end
+  
+  describe "save ssr" do
+    it "should save ssr" do
+      a_flight = @user.arrival_flights.create(@attr)
+      a_flight.ssr = "Chuyen bay nhieu VIP/CIP"
+      a_flight.save
+      a_flight.ssr.should == "Chuyen bay nhieu VIP/CIP"
+    end
+  end
+  
+  describe "adjust arrival time" do
+    it "should not do anything if eta arrnextday nil" do
+      a_flight = @user.arrival_flights.create(@attr)
+      time = a_flight.eta
+      a_flight.eta
+      a_flight.eta_arrnextday = nil
+      a_flight.save!
+      a_flight.eta.localtime.should == time
+    end
+    
+    it "should adjust 1 day if arrnextday true" do
+      a_flight = @user.arrival_flights.create(@attr)
+      time = a_flight.eta
+      a_flight.eta_arrnextday = true
+      a_flight.save!
+      a_flight.eta.should == time.advance(:days => 1)
     end
   end
 end
