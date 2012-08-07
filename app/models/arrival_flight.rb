7d2e4ceab02c942b23a876885f950738e14361a1
Linh Mai-Office <linhmt@vietnamairlines.com>
2012-08-07 17:30:41 +0700

    add lnf_user_id, generate outbound, fixed arrival next date

--- app/models/arrival_flight.rb
+++ app/models/arrival_flight.rb
@@ -10,6 +10,10 @@ class ArrivalFlight < ActiveRecord::Base
   before_save :update_internal_attributes
   after_save :generate_outbound
   
+  scope :assigned_flights, lambda { |user|
+    where("(arrival_flights.user_id = ? or arrival_flights.lnf_user_id = ?) AND arrival_flights.flight_date = DATE(NOW())",
+      user.id, user.id)
+  } 
   def self.arrival_flights(date, is_domestic, page)
     flight_date = ArrivalFlight.retrieve_flight_date(date)
     condition = {
@@ -30,7 +34,7 @@ class ArrivalFlight < ActiveRecord::Base
     }
     ArrivalFlight.where(condition).page(page).per(50)
   end
-  
+
   def update_internal_attributes
     self.flight_no = self.flight_no.upcase
     self.is_domestic = update_is_domestic
@@ -39,19 +43,19 @@ class ArrivalFlight < ActiveRecord::Base
     self.ata = adjust_arrival_time(self.flight_date, self.ata, self.ata_arrnextday)
     true
   end
-    
+
   def adjust_arrival_time(date_t, time_t, arrnextday)
     if (!time_t.nil?)
-      if (!arrnextday.nil? && arrnextday)
+      if (!arrnextday.nil? && arrnextday == "1")
         time_temp = Time.new(date_t.year, date_t.month, date_t.day, time_t.hour, time_t.min)
         adjusted_time = time_temp.advance(:days => +1)
       else
-        adjusted_time = Time.new(date_t.year, date_t.month, date_t.day, time_t.hour, time_t.min).utc
+        adjusted_time = Time.new(date_t.year, date_t.month, date_t.day, time_t.hour, time_t.min)
       end
     end
     adjusted_time
   end
-  
+
   def update_is_domestic
     temp = self.flight_no.slice(2)
     domestic = (self.flight_no.length == 6 && (temp.to_i == 1 || temp.to_i == 7))
@@ -66,25 +70,59 @@ class ArrivalFlight < ActiveRecord::Base
     a_date = Time.zone.parse(date)
     a_date
   end
-  
+
   def approval_flight(current_user)
     self.is_approval = true
     self.approval_by = current_user.id
     self.save!
   end
-  
+
   private
+  def process_outbound_manual(outbound_tags)
+    outbound = strip_tags(outbound_tags)
+    outbound_list = outbound.split(/\r\n/)
+    outbound_list.each do |item|
+      item = item.gsub(/\s+/, '')
+      items = item.split(/[\\\/]/)
+      if (items.size == 2)
+        ot = Outbound.find_or_initialize_by_flight_no_and_arrival_flight_id(items[0].upcase, self.id)
+        ot.update_attribute(:pax_number, items[1])
+      end
+    end
+  end
+  
+  def process_outbound_text(outbound_text)
+    temp_list = outbound_text.gsub(/&nbsp;|\s+|&yen/, '').split(/<br\s*\/>/)
+    temp_list = temp_list.select{|e| e.match(/^[0-9]/)}
+    outbound_hash = Hash.new
+    temp_list.each do |ot_pax|
+      ot_pax = strip_tags(ot_pax)
+      ot_pax = ot_pax.split(/\.\.+/)
+      if (ot_pax[2].length == 14)
+        flt = ot_pax[2][0..5]
+        if outbound_hash.has_key?(flt)
+          outbound_hash[flt] += [ot_pax[0] + '--' + ot_pax[4]]
+        else
+          outbound_hash[flt] = [ot_pax[0] + '--' + ot_pax[4]]
+        end
+      end
+    end  
+    outbound_hash
+  end
+  
+  def update_flight_outbounds(outbound_hash = {})
+    outbound_hash.each_pair { |key, value|
+      ot = Outbound.find_or_initialize_by_flight_no_and_arrival_flight_id(key.gsub(/\./,'').upcase,self.id)
+      ot.update_attribute(:pax_number, value.length)}
+  end
+    
   def generate_outbound
     unless outbound_tags.nil?
-      outbound = strip_tags(outbound_tags)
-      outbound_list = outbound.split(/\r\n/)
-      outbound_list.each do |item|
-        item = item.gsub(/\s+/, '')
-        items = item.split(/[\\\/]/)
-        if (items.size == 2)
-          ot = Outbound.find_or_initialize_by_flight_no_and_arrival_flight_id(items[0].upcase, self.id)
-          ot.update_attribute(:pax_number, items[1])
-        end
+      if outbound_tags.match(/<ul\>|<li\>/)
+        process_outbound_manual(outbound_tags)
+      else
+        outbound_hash = process_outbound_text(outbound_tags)
+        update_flight_outbounds(outbound_hash)
       end
     end
   end
