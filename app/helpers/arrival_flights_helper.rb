f4d2cf4f4d35b134ccfcedc32253e561e846bf7f
Linh Mai-Office <linhmt@vietnamairlines.com>
2012-08-07 17:31:05 +0700

    add generate further information

--- app/helpers/arrival_flights_helper.rb
+++ app/helpers/arrival_flights_helper.rb
@@ -3,4 +3,18 @@ module ArrivalFlightsHelper
     a = s.split(/\s/) # or /[ ]+/ to only split on spaces
     a[0...n].join(' ') + (a.size > n ? '...' : '')
   end
+  
+  def generate_further_information(arrival)
+    f_info = ""
+    if (!arrival.remarks.nil?)
+      f_info = f_info + "RMK."
+    end
+    if ((!arrival.eta.nil? && (arrival.eta > arrival.sta)) || (!arrival.ata.nil? && (arrival.ata > arrival.sta)))
+      f_info = f_info + "DLY."
+    end
+    if arrival.is_approval
+      f_info = f_info + "Closed."
+    end
+    f_info
+  end
 end
