3f720a14a22fb071470cd566cfaf74ecd9725fbb
Linh Mai-Office <linhmt@vietnamairlines.com>
2012-08-22 15:57:45 +0700

    new work

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,8 +26,8 @@ Dvhk::Application.configure do
   :password             => 'hongha27',
   :authentication       => 'plain',
   :enable_starttls_auto => true  }
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = false
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
 
   # Print deprecation notices to the Rails logger
@@ -40,6 +40,6 @@ Dvhk::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = false
+  config.assets.debug = true
 
 end
