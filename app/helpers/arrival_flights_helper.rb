d3ec77805981ee9b21f938b2f135d8b1417f883c
Linh Mai-Office <linhmt@vietnamairlines.com>
2012-09-26 16:44:02 +0700

    arrival flight

--- app/helpers/arrival_flights_helper.rb
+++ app/helpers/arrival_flights_helper.rb
@@ -7,22 +7,25 @@ module ArrivalFlightsHelper
   def generate_further_information(arrival)
     f_info = ""
     if (arrival.baggage_assessment == true)
-      f_info = f_info + "DG."
+      f_info = f_info + "DG| "
     end
     if (arrival.outbounds.size > 0)
-      f_info = f_info + "OB."
+      f_info = f_info + "OB| "
+    end
+    unless (arrival.baggage.blank?)
+      f_info = f_info + "BG| "
     end
     if (arrival.remarks)
-      f_info = f_info + "RMK."
+      f_info = f_info + "Irregular| "
     end
     if ((arrival.eta && (arrival.eta > arrival.sta)) || (arrival.ata && (arrival.ata > arrival.sta)))
-      f_info = f_info + "DLY."
+      f_info = f_info + "DLY| "
     end
     if arrival.is_approval
-      f_info = f_info + "Closed."
+      f_info = f_info + "Closed| "
     end
     if (arrival.notify_count && arrival.notify_count > 0)
-      f_info = f_info + "CR."
+      f_info = f_info + "CR| "
     end
     f_info
   end
