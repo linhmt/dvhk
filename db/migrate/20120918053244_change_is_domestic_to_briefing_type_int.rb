f30ff75e1e3f233b7525ad575bd641afc2ac87df
Linh PC2 QNDi <linhmt@vietnamairlines.com>
2012-10-07 21:24:25 +0700

    change briefingpost content limit to 1000

--- db/migrate/20120918053244_change_is_domestic_to_briefing_type_int.rb
+++ db/migrate/20120918053244_change_is_domestic_to_briefing_type_int.rb
@@ -1,9 +1,11 @@
 class ChangeIsDomesticToBriefingTypeInt < ActiveRecord::Migration
   def up
     change_column :briefingposts, :is_domestic, :integer, :limit => 2, :default => 2
+    change_column :briefingposts, :context, :text, :limit => 1000
   end
 
   def down
     change_column :briefingposts, :is_domestic, :boolean
+    change_column :briefingposts, :context, :text
   end
 end
