36ecbf9ed7d764a1767b2a7106a082b836de395a
Linh PC2 QNDi <linhmt@vietnamairlines.com>
2012-10-07 21:26:50 +0700

    fix typo :content

--- db/migrate/20120918053244_change_is_domestic_to_briefing_type_int.rb
+++ db/migrate/20120918053244_change_is_domestic_to_briefing_type_int.rb
@@ -1,11 +1,11 @@
 class ChangeIsDomesticToBriefingTypeInt < ActiveRecord::Migration
   def up
     change_column :briefingposts, :is_domestic, :integer, :limit => 2, :default => 2
-    change_column :briefingposts, :context, :text, :limit => 1000
+    change_column :briefingposts, :content, :text, :limit => 1000
   end
 
   def down
     change_column :briefingposts, :is_domestic, :boolean
-    change_column :briefingposts, :context, :text
+    change_column :briefingposts, :content, :text
   end
 end
