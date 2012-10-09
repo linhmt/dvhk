8e386b6494d55fe35282805b719c5226d7f6fa02
Linh PC2 QNDi <linhmt@vietnamairlines.com>
2012-10-09 18:35:43 +0700

    do not remove is_general

--- db/migrate/20120918053244_change_is_domestic_to_briefing_type_int.rb
+++ db/migrate/20120918053244_change_is_domestic_to_briefing_type_int.rb
@@ -1,13 +1,11 @@
 class ChangeIsDomesticToBriefingTypeInt < ActiveRecord::Migration
   def up
     change_column :briefingposts, :is_domestic, :integer, :limit => 2, :default => 2
-    remove_column(:briefingposts, :is_general)
     change_column :briefingposts, :content, :text, :limit => 1000
   end
 
   def down
     change_column :briefingposts, :is_domestic, :boolean
-    add_column(:briefingposts, :is_general, :boolean)
     change_column :briefingposts, :content, :text
   end
 end
